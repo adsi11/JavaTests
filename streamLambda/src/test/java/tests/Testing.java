07405b053b30514479de54db9b41c498850401e7
Fernando Peña <lpeaflo1@itam.mx>
2018-03-15 09:34:40 -0600

    Bug on test finished

--- streamLambda/src/test/java/tests/Testing.java
+++ streamLambda/src/test/java/tests/Testing.java
@@ -5,29 +5,27 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.junit.Test;
+
 public class Testing {
-	
-	public static void main(String[] args) {
-		try {
-			int i;
-			int totalJugadores;
-			for (i = 1; i <= 1000; i++) {
-				totalJugadores = ThreadLocalRandom.current().nextInt(0, 10000);
-				Main m = new Main(totalJugadores);
-				m.sortIdAsc();
-				assertTrue(TestMethods.isOrderedIdAsc(m.getNumbers()));
-				m.sortIdDesc();
-				assertTrue(TestMethods.isOrderedIdDesc(m.getNumbers()));
-				m.sortRatingAsc();
-				assertTrue(TestMethods.isOrderedRatingAsc(m.getNumbers()));
-				m.sortRatingDesc();
-				assertTrue(TestMethods.isOrderedRatingDesc(m.getNumbers()));
-				assertTrue(TestMethods.getPromedio(m.getNumbers()) == m.average());
-				System.out.println("Test " + i + " passed");
-			}
-			System.out.println("Se pasaron las " + (i - 1) + " pruebas. Todas con número de jugadores aleatorio entre 0 y 10000.");
-		} catch (Exception e) {
-			System.out.println("Test not passed");
+	@Test
+	public void t() {
+		int i;
+		int totalJugadores;
+		for (i = 1; i <= 1000; i++) {
+			totalJugadores = ThreadLocalRandom.current().nextInt(0, 10000);
+			Main m = new Main(totalJugadores);
+			m.sortIdAsc();
+			assertTrue(TestMethods.isOrderedIdAsc(m.getNumbers()));
+			m.sortIdDesc();
+			assertTrue(TestMethods.isOrderedIdDesc(m.getNumbers()));
+			m.sortRatingAsc();
+			assertTrue(TestMethods.isOrderedRatingAsc(m.getNumbers()));
+			m.sortRatingDesc();
+			assertTrue(TestMethods.isOrderedRatingDesc(m.getNumbers()));
+			assertTrue(TestMethods.getPromedio(m.getNumbers()) == m.average());
+			System.out.println("Test " + i + " passed");
 		}
+		System.out.println("Se pasaron las " + (i - 1) + " pruebas. Todas con número de jugadores aleatorio entre 0 y 10000.");
 	}
 }
